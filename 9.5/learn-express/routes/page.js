f4df42277797806fa2b60702c4c4bfeac5e2ce06
hajune <blackjune@kakao.com>
2022-06-17 22:35:33 +0900

    express structure settings

--- 9.5/learn-express/routes/page.js
+++ 9.5/learn-express/routes/page.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-
-    console.log('==> test');
+router.use((req, res, next) => {
+    res.locals.use = null;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerIdList = [];
     next();
 });
 
-router.get('/', (req, res, next) => {
-    res.render('main', () => {
+router.get('/profile', (req, res, next) => {
+    console.log('==> test');
+    res.render('profile', { title: '내정보' });
+});
 
+router.get('/join', (req, res) => {
+    res.render('join', { title: '회원가입' });
+});
+
+router.get('/', (req, res, next) => {
+    const twits = [];
+    res.render('main', {
+        title: 'june',
+        twits,
     });
 });
+
+module.exports = router;
