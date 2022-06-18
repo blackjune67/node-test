62b9c025b809c3ca1f1b17d8962b5447f4dd1f73
hajune <blackjune@kakao.com>
2022-06-18 23:28:29 +0900

    db sequelize create

--- 9.5/learn-express/models/index.js
+++ 9.5/learn-express/models/index.js
@@ -1,21 +1,40 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+
+// * 설정파일, NODE_ENV 없으면 development 기본값으로 되게끔.
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
+
+const User = require('./user');
+const Post = require('./hashtag');
+const Hashtag = require('./hashtag');
+
+// const basename = path.basename(__filename);
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+
+/*if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+}*/
 
-fs
+db.sequelize = sequelize;
+db.User = User;
+db.Post = Post;
+db.Hashtag = Hashtag;
+
+User.init(sequelize);
+Post.init(sequelize);
+Hashtag.init(sequelize);
+
+User.association(db);
+Post.association(db);
+Hashtag.association(db);
+
+/*fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
@@ -23,15 +42,15 @@ fs
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
-  });
+  });*/
 
-Object.keys(db).forEach(modelName => {
+/*Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
 db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.Sequelize = Sequelize;*/
 
 module.exports = db;
